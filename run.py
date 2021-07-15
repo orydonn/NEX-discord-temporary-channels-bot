03aef4c903e52d600beaf24a19ef65eaa769b26f
aksionov_ilia_2 <L0m0n0$0V>
2021-07-15 23:19:07 +0300

    Готовая версия

--- run.py
+++ run.py
@@ -9,10 +9,9 @@ TOKEN = 'ODU0OTc4NzcyMDcwNjk0OTEz.YMrzBw.YYYiiYEnUkp0H38DZUaqmn0s4L4'
 
 bot = commands.Bot(command_prefix='!')
 
-created_channel = None
-maincategory = None
+maincategory = 865318947499016192
 
-channels = []
+channels = {}
 
 
 class temporary_channel:
@@ -23,30 +22,32 @@ class temporary_channel:
 
     def checking_data(self):
         if self.creator not in database:
-            database.setdefault(self.creator, [str(self.creator.name), maincategory, {'bitrate': None, 'user_limit': 5, 'rtc_region': None}])
+            database.setdefault(self.creator, [str(self.creator.name), self.channel_category, {'bitrate': None, 'user_limit': 5, 'rtc_region': None}])
 
-    def creating_channel(self):
+    async def creating_channel(self):
         for guild in bot.guilds:
             self.created_channel = await guild.create_voice_channel(name=database[self.creator][0], category=database[self.creator][1], **database[self.creator][2])
         await self.creator.move_to(self.created_channel)
 
     def updating_data(self):
         new_setting = bot.get_channel(self.created_channel.id)
-        print('Ливнул')
-        database[self.creator] = [new_setting.name, maincategory,
+        database[self.creator] = [new_setting.name, self.channel_category,
                             {'bitrate': new_setting.bitrate, 'user_limit': new_setting.user_limit, 'rtc_region': None}]
-        print(*database[self.creator])
 
-    def checking_void(self):
-        await created_channel.delete()
-        print(self.created_channel)
+    async def checking_void(self, channel):
+        await channel.delete()
+        print(channels_for_delete, '--------------------------')
+        return [self.creator]
 
 
 @bot.event
 async def on_voice_state_update(member, before, after):
     if after.channel is not None:
-        if after.channel.id == 864122465638678588:
-            print('Зашел')
+        if after.channel.id == 861291318612590632:
+            if member not in channels:
+                channels.setdefault(member, temporary_channel(maincategory, member))
+            channels[member].checking_data()
+            await channels[member].creating_channel()
 
             # global maincategory
             # maincategory = bot.get_channel(864122028306071583)
@@ -60,14 +61,23 @@ async def on_voice_state_update(member, before, after):
 
 
     else:
-        if before.channel.id == created_channel.id:
+        for created_channel in channels.items():
+            if before.channel.id == created_channel[1].created_channel.id:
+                created_channel[1].updating_data()
             # new_setting = bot.get_channel(created_channel.id)
             # print('Ливнул')
             # database[member] = [new_setting.name, maincategory, {'bitrate': new_setting.bitrate, 'user_limit': new_setting.user_limit, 'rtc_region': None}]
             # print(*database[member])
-        if bot.get_channel(created_channel.id).members == []:
             # await created_channel.delete()
             # print(created_channel)
+    global channels_for_delete
+    channels_for_delete = []
+    for created_channel in channels.items():
+        if created_channel[1].created_channel.members == []:
+            channels_for_delete.append(await created_channel[1].checking_void(created_channel[1].created_channel))
+    for deleting_channel in channels_for_delete:
+        print(deleting_channel[0])
+        channels.pop(deleting_channel[0])
 
 
 bot.run(TOKEN)
