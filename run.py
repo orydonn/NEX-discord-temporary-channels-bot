c459dd780900f2cb6a187074f25755c097adbbe3
aksionov_ilia_2 <L0m0n0$0V>
2022-10-04 14:50:32 +0300

    NEX 1.0

--- run.py
+++ run.py
@@ -1,40 +1,52 @@
-import discord
 from discord.ext import commands
-import random
-from discord.utils import get
-import asyncio
+
+from settings import *
 
 database = {}
-TOKEN = 'ODU0OTc4NzcyMDcwNjk0OTEz.YMrzBw.YYYiiYEnUkp0H38DZUaqmn0s4L4'
 
+# unic discord bot token
+TOKEN = TOKEN
+
+# id of your mother channel, with the usage of which will be created temporary channels will be created
+mother_channel_id = mother_channel
+
+# choosing prefix for using bot's commands
 bot = commands.Bot(command_prefix='!')
 
-maincategory = 865318947499016192
+# id of category, where temporary channels will be placed
+main_category = main_category_id
 
+# initialising of the dict for saving temporary channels
 channels = {}
 
 
+# class of the temporary channel
 class temporary_channel:
     def __init__(self, category, creator):
-        self.created_channel = None
-        self.creator = creator
-        self.channel_category = bot.get_channel(category)
+        self.created_channel = None  # object of the voice channel
+        self.creator = creator  # nickname of the channel creator
+        self.channel_category = bot.get_channel(category)  # the category where channel will be placed
 
-    def checking_data(self):
+    def checking_data(self):  # checking for the existence of the channel in database(will be added soon)
         if self.creator not in database:
-            database.setdefault(self.creator, [str(self.creator.name), self.channel_category, {'bitrate': None, 'user_limit': 5, 'rtc_region': None}])
+            database.setdefault(self.creator.name, [str(self.creator.name), self.channel_category,
+                                                    {'bitrate': None, 'user_limit': 5, 'rtc_region': None}])
+            # if channel not in database, it is adding there
 
-    async def creating_channel(self):
+    async def creating_channel(self):  # creating of the voice channel and connecting it to rhe temporary object
         for guild in bot.guilds:
-            self.created_channel = await guild.create_voice_channel(name=database[self.creator][0], category=database[self.creator][1], **database[self.creator][2])
-        await self.creator.move_to(self.created_channel)
+            self.created_channel = await guild.create_voice_channel(name=database[self.creator.name][0],
+                                                                    category=database[self.creator.name][1],
+                                                                    **database[self.creator.name][2])
+        await self.creator.move_to(self.created_channel)  # moving the user to newly created channel
 
-    def updating_data(self):
+    def updating_data(self):  # updating of the settings, if they have been changed
         new_setting = bot.get_channel(self.created_channel.id)
-        database[self.creator] = [new_setting.name, self.channel_category,
-                            {'bitrate': new_setting.bitrate, 'user_limit': new_setting.user_limit, 'rtc_region': None}]
+        database[self.creator.name] = [new_setting.name, self.channel_category,
+                                       {'bitrate': new_setting.bitrate, 'user_limit': new_setting.user_limit,
+                                        'rtc_region': None}]
 
-    async def checking_void(self, channel):
+    async def checking_void(self, channel):  # checking for the void in the channel
         await channel.delete()
         print(channels_for_delete, '--------------------------')
         return [self.creator]
@@ -43,38 +55,22 @@ class temporary_channel:
 @bot.event
 async def on_voice_state_update(member, before, after):
     if after.channel is not None:
-        if after.channel.id == 861291318612590632:
+            if after.channel.id == mother_channel_id:
             if member not in channels:
-                channels.setdefault(member, temporary_channel(maincategory, member))
+                channels.setdefault(member, temporary_channel(main_category, member))
             channels[member].checking_data()
             await channels[member].creating_channel()
-
-            # global maincategory
-            # maincategory = bot.get_channel(864122028306071583)
-            # if member not in database:
-            #     database.setdefault(member, [str(member.name), maincategory, {'bitrate': None, 'user_limit': 5, 'rtc_region': None}])
-            # print(*database[member])
-            # for guild in bot.guilds:
-            #     global created_channel
-            #     created_channel = await guild.create_voice_channel(name=database[member][0], category=database[member][1], **database[member][2])
-            # await member.move_to(created_channel)
-
 
     else:
         for created_channel in channels.items():
             if before.channel.id == created_channel[1].created_channel.id:
                 created_channel[1].updating_data()
-            # new_setting = bot.get_channel(created_channel.id)
-            # print('Ливнул')
-            # database[member] = [new_setting.name, maincategory, {'bitrate': new_setting.bitrate, 'user_limit': new_setting.user_limit, 'rtc_region': None}]
-            # print(*database[member])
-            # await created_channel.delete()
-            # print(created_channel)
     global channels_for_delete
     channels_for_delete = []
-    for created_channel in channels.items():
-        if created_channel[1].created_channel.members == []:
-            channels_for_delete.append(await created_channel[1].checking_void(created_channel[1].created_channel))
+    for created_channel in channels.values():
+        print(f'{created_channel.created_channel.members} в канале {created_channel}')
+        if not created_channel.created_channel.members:
+            channels_for_delete.append(await created_channel.checking_void(created_channel.created_channel))
     for deleting_channel in channels_for_delete:
         print(deleting_channel[0])
         channels.pop(deleting_channel[0])
